a6dabd40b3d0ab068e4e59c18d59fbf98f360db1
MallamTY <sosanyatemitayonurudeen@gmail.com>
2023-04-02 14:11:20 +0100

    Updated ReadMe

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: "../.env"});
 
+console.log(process.env.ALTER);
 
 
 export const CONNECT_STRING: string = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}`;
